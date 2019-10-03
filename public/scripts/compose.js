07b3aafb164f40cc2fcb818c3e90734056796643
Adhish Sharma <adhishsharma0@gmail.com>
2019-10-03 13:10:15 +0530

    fixed posts position on homepage

--- public/scripts/compose.js
+++ public/scripts/compose.js
@@ -5,8 +5,7 @@ while (s!= "rocknroll")
 s=prompt("Please Enter Your Password");
 if (s=="rocknroll")
 {
-    res.redirect("/compose")
-// window.location.href="compose.ejs"; //page to redirect if password entered is correct
+    res.redirect("/compose");
 }
 else
 {
